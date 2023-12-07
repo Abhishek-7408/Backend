ba6842f618ad55884818aa5acf95a7e4962a0f01
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2023-12-07 20:48:33 +0530

    Some basic file structures

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,10 +1,29 @@
 const http = require('http');
+const { type } = require('os');
 const port =8000;
+const fs = require('fs');
 
 
 function requestHandler(req,res){
     console.log(req.url);
-    res.end('Gotcha!');
+    
+    
+    res.writeHead(200,{'content-type': 'text/html'});
+   
+    fs.readFile('./index.html',function(err,data){
+        if (err){
+            console.log('error',err);
+            return res.end('<>Error!</h1>');
+
+        }
+
+return res.end(data);
+
+    });
+   
+   
+   
+    // res.end('<h1>Gotcha!</h1>');
 
 }
 
